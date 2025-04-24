b38bbfd8aca2a25632e8becfceb3b9542ee0351f
Kim-ByeongGyu <zenk5554785@gmail.com>
2025-04-24 15:05:03 +0900

    first commit

---
+++ src/main/java/org/example/parkme/ParkMeApplication.java
@@ -0,0 +1,13 @@
+package org.example.parkme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/parkme/ParkMeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parkme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
